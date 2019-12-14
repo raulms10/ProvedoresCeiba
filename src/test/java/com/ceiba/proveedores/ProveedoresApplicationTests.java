c5a885846db10b17c3953a36d2659640acd801d2
raulms10 <m.raulio10@gmail.com>
2019-12-14 09:23:44 -0500

    Init project

---
+++ src/test/java/com/ceiba/proveedores/ProveedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceiba.proveedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProveedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
